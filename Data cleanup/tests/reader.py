8e0b51123e3bcc6a91d815ce52190384ad3a9ca7
Mert Erol <meroert55@gmail.com>
2024-03-19 16:56:26 +0100

    comparing pandas and polars

--- Data cleanup/tests/reader.py
+++ Data cleanup/tests/reader.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 AUP = "data/cleaned/AmongUsPorn_submissions_cleaned.csv" # AmongUsPorn Data set with 1.5M data points
 
-df = pl.read_csv(AUP, truncate_ragged_lines=True, ignore_errors=True)
-df = df.drop_nulls()
+df_polars = pl.read_csv(AUP, truncate_ragged_lines=True, ignore_errors=True)
+df_polars = df_polars.drop_nulls()
 
-# TAKES A VERY LONG TIME - BE PATIENT
+df_pandas = pd.read_csv(AUP, skip_blank_lines=True, error_bad_lines=False, warn_bad_lines=False)
+df_pandas = df_pandas.dropna()
 
-print(df.describe())
-#print(df.summary())
+
+print(df_polars.describe())
+print(df_pandas.summary())
