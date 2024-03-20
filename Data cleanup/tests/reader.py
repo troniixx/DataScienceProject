a5227811d0a778e3ef3e38326b5d7134d7305395
Mert Erol <meroert55@gmail.com>
2024-03-20 10:40:27 +0100

    added requirements

--- Data cleanup/tests/reader.py
+++ Data cleanup/tests/reader.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 AUP = "data/cleaned/AmongUsPorn_submissions_cleaned.csv" # AmongUsPorn Data set with 1.5M data points
 
-df_polars = pl.read_csv(AUP, truncate_ragged_lines=True, ignore_errors=True)
-df_polars = df_polars.drop_nulls()
+# df_polars = pl.read_csv(AUP, truncate_ragged_lines=True, ignore_errors=True)
+# df_polars = df_polars.drop_nulls()
 
 df_pandas = pd.read_csv(AUP, skip_blank_lines=True, error_bad_lines=False, warn_bad_lines=False)
 df_pandas = df_pandas.dropna()
 
 
-print(df_polars.describe())
+#print(df_polars.describe())
+
 print(df_pandas.summary())
